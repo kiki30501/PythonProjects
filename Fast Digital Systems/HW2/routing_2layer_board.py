66ac557ccc12d006fb5b4e384e849b7e526342c9
Reshef Schachter <sixsixbest@gmail.com>
2024-06-10 21:07:30 +0300

    Added a jupyter notebook for ease of use

--- Fast Digital Systems/HW2/routing_2layer_board.py
+++ Fast Digital Systems/HW2/routing_2layer_board.py
@@ -1,13 +1,47 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Create a 3D array of size 25x25x2
-array_3d = np.zeros((25, 25, 2), dtype=np.uint8)
+plt.style.use('_mpl-gallery')
 
-# Example: Set the value of an element at index (10, 10, 1) to 3 (binary: 11)
-array_3d[10, 10, 1] = 3
+# Create a 3D array of size 25x25x2 initialized with zeros
+board  = np.zeros((25, 25, 2), dtype=np.uint8)
 
-# Example: Get the value of an element at index (10, 10, 1)
-value = array_3d[10, 10, 1]
+# choose 10 unique random points on the board on the first layer but only between 1 and 23
+points = np.random.choice(np.arange(1, 23), size=(10, 2), replace=False)
 
-print(array_3d)
-print(value)
+# assign 5 points to the origins and 5 points to the destinations
+origins_2d = points[:5]
+destins_2d = points[5:]
+
+# expand the 2D points to 3D points by adding the layer index to be 0 (will be useful in the routing check operations)
+origins    = np.hstack((origins_2d, np.zeros((5, 1), dtype=np.uint8)))
+destins    = np.hstack((destins_2d, np.zeros((5, 1), dtype=np.uint8)))
+
+# assign the origins and destinations to the board as 1
+board[origins[:, 0], origins[:, 1], 0] = 1
+board[destins[:, 0], destins[:, 1], 0] = 1
+board[0,  :,  :] = 1
+board[24, :,  :] = 1
+board[:,  0,  :] = 1
+board[:,  24, :] = 1
+
+# visualize layer 0 of the board
+plt.figure(figsize=(8,8))
+plt.imshow(board[:, :, 0])
+plt.xlim(-0.5, 24.5)
+plt.ylim(-0.5, 24.5)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.scatter(origins[:, 1], origins[:, 0], c='r', label='Origins')
+plt.scatter(destins[:, 1], destins[:, 0], c='g', label='Destinations')
+plt.grid(False)
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+print("Origins:\n", origins, "\n")
+print("Destinations:\n", destins, "\n")
+print("Points:\n", points)
+
+
